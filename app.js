cbd838a33feec34b95585b5ce56d5d14afdf8a0d
gecko404 <jean-baptiste.abet@live.fr>
2020-04-24 18:27:43 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,120 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var config = require('./config');
+var fs = require('fs');
+
+var date = new Date();
+let peopleInside = 0;
+
+fs.readdir('./log/', (err, files) => {
+  if(files == ''){
+    peopleInside = 0;
+    return true;
+  }
+  let j = 1;
+  let filePath = files[files.length - 1].split('-').slice(-1) == 'ERROR.log' ? files[files.length - 2] : files[files.length - 1];
+  fs.readFile(`./log/${filePath}`, 'utf8', function(err, contents) {
+    let i = 1;
+    if(contents == ''){
+      peopleInside = 0;
+      return true;
+    }
+    while(contents.split('\n').slice(-i-1, -i)[0].split(" ").includes('{"message":"ERROR') || contents.split('\n').slice(-i-1, -i)[0].split(" ").includes('exitedWhileDisconnected')){
+      i++;
+      if(i > contents.split('\n').length-2){
+        peopleInside = 0;
+        return true;
+      }
+    }
+    if (!contents.split('\n').slice(-i-1, -i)[0].split(" ").includes('{"message":"ERROR')) {
+      let array = contents.split('\n').slice(-i-1, -i)[0].split(" ");
+      peopleInside = Number(array[array.findIndex((e) => e == 'people') - 1]);
+      peopleInside = peopleInside < 0  ? 0 : peopleInside;
+    }
+  });
+});
+
+var winston = require('winston');
+require('winston-daily-rotate-file');
+
+var logger = winston.createLogger({
+    transports: [
+      new (winston.transports.DailyRotateFile)({
+          level: 'info',
+          frequency: '30m',
+          filename: '%DATE%-COMBINED.log',
+          datePattern: 'YYYY-MM-DD-HH[h]mm',
+          dirname: './log'
+        }),
+      new (winston.transports.DailyRotateFile)({
+          level: 'error',
+          frequency: '30m',
+          filename: '%DATE%-ERROR.log',
+          datePattern: 'YYYY-MM-DD-HH[h]mm',
+          dirname: './log'
+        }),
+    ]
+  });
+
+const checkMaxPeopleInside = () => {
+  if(peopleInside > config.maxPeopleInside){
+    const date = new Date();
+    logger.error(`ERROR : Max people inside (${config.maxPeopleInside}) exceeded : ${peopleInside} people inside at ${date.toString()}`)
+  }
+}
+const checkMinPeopleInside = () =>{
+  if(peopleInside < 0){
+    const date = new Date();
+    logger.error(`ERROR : Less than 0 people inside at ${date.toString()}`)
+    peopleInside = 0;
+  }
+}
+
+app.get('/', function(req, res){
+  res.send('hello');
+});
+
+io.on('connection', (socket) => {
+  socket.emit('peopleInside', {peopleInside});
+  socket.emit('maxPeopleInside', config.maxPeopleInside);
+
+  socket.on('+', function (data) {
+    if(data != undefined){
+      if(data == config.password){
+        const date = new Date();
+        peopleInside = peopleInside + 1
+        socket.emit('peopleInside', {"peopleInside": peopleInside});
+        socket.broadcast.emit('peopleInside', {"peopleInside": peopleInside});
+        logger.info(`${date.toString()} / enter / ${peopleInside} people inside`);
+        checkMaxPeopleInside();
+      }
+    }
+  });
+
+  socket.on('-', function (data) {
+    if(data != undefined){
+      if(data == config.password){
+        const date = new Date();
+        peopleInside = peopleInside - 1
+        checkMinPeopleInside();
+        socket.emit('peopleInside', {"peopleInside": peopleInside});
+        socket.broadcast.emit('peopleInside', {"peopleInside": peopleInside});
+        logger.info(`${date.toString()} / exit / ${peopleInside} people inside`);
+      }
+    }
+  });
+
+  socket.on('exitedWhileDisconnected', function (data) {
+    peopleInside = peopleInside - data;
+    logger.info(`${date.toString()} / exitedWhileDisconnected / ${data} exited while server was disconnected. Now ${peopleInside} people inside`);
+    checkMinPeopleInside();
+    socket.emit('peopleInside', {peopleInside});
+    socket.broadcast.emit('peopleInside', {"peopleInside": peopleInside});
+  });
+
+});
+
+http.listen(config.port, function(){
+
+});
---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  password :  'password',
+  port : 3000,
+  maxPeopleInside : 20,
+};
